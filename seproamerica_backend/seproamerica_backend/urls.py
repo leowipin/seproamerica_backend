e2bc9a9e1b5a32e58d1c2f27d42792aacf71759b
leowipin <seproamerica2022@4146c987a9a6>
2023-03-25 14:18:59 +0000

    static files modified

--- seproamerica_backend/seproamerica_backend/urls.py
+++ seproamerica_backend/seproamerica_backend/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('equipment/', include('equipment.urls')),
     path('services/', include('services.urls')),
+    path('adminC/', TemplateView.as_view(template_name='static/adminC/index.html')),
 ]
