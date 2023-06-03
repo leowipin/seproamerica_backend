87454d2140544c90bbdf6e41d58c77a91e791fc6
leowipin <leonardopincayjaramillo@gmail.com>
2023-06-03 18:54:16 -0500

    modified TIME_ZONE to America/Guayaquil & new date_sended field in notifications

--- seproamerica_backend/notifications/models.py
+++ seproamerica_backend/notifications/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from users.models import Cliente
 from services.models import Pedido
+from datetime import date
 
 # Create your models here.
 
@@ -21,9 +22,11 @@ class OrderClientNotification(models.Model):
     title = models.CharField(max_length=255)
     message = models.TextField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_sended = models.DateField(default=date.today)
 
 class OrderAdminNotification(models.Model): # para el topic 'administrador'
     title = models.CharField(max_length=255)
     message = models.TextField()
     order = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    date_sended = models.DateField(default=date.today)
 
