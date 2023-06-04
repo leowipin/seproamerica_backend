aea8bdd46e5b13ecc4047250516cfe5db724c8dc
leowipin <leonardopincayjaramillo@gmail.com>
2023-06-04 13:46:23 -0500

    modified admin file in notifications

--- seproamerica_backend/notifications/admin.py
+++ seproamerica_backend/notifications/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import TokenFCM, OrderAdminNotification
+from .models import TokenFCM, OrderAdminNotification, OrderClientNotification
 # Register your models here.
 
 class FCMTokenAdmin(admin.ModelAdmin):
     list_display = ('id','user', 'created_at')
 
-class OrderAndminNotificationAdmin(admin.ModelAdmin):
-    list_display = ('id','title', 'message', 'order')
+class OrderAdminNotificationAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'message', 'order', 'date_sended')
+
+class OrderClientNotificationAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'message', 'user', 'date_sended')
 
 admin.site.register(TokenFCM, FCMTokenAdmin)
-admin.site.register(OrderAdminNotification, OrderAndminNotificationAdmin)
+admin.site.register(OrderAdminNotification, OrderAdminNotificationAdmin)
+admin.site.register(OrderClientNotification, OrderClientNotificationAdmin)
