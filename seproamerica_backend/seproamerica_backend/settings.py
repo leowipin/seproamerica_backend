4a6d3ff317472519579d2f198f42d442f2ee57b7
leowipin <leonardopincayjaramillo@gmail.com>
2023-06-20 12:57:40 -0500

    correction static file in settings

--- seproamerica_backend/seproamerica_backend/settings.py
+++ seproamerica_backend/seproamerica_backend/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [BASE_DIR]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
